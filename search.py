0116825057d27d6c35aad35f57888e7845787f68
thiadeliria <spec.thia@gmail.com>
2018-10-08 21:06:19 +0800

    Cleaned up search.py comments.

--- search.py
+++ search.py
@@ -13,7 +13,7 @@
 
 
 """
-In search.py, you will implement generic search algorithms which are called by
+In search.py, we implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
 """
 
@@ -23,8 +23,6 @@ class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
-
-    You do not need to change anything in this class, ever.
     """
 
     def getStartState(self):
@@ -73,20 +71,7 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
+    """Search the deepest nodes in the search tree first."""
 
     #to be explored (FIFO). holds nodes (state, action[])
     frontier = util.Stack()
@@ -122,7 +107,6 @@ def depthFirstSearch(problem):
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
 
     #to be explored (FIFO)
     frontier = util.Queue()
@@ -160,7 +144,6 @@ def breadthFirstSearch(problem):
         
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
 
     #to be explored (FIFO): holds (item, cost)
     frontier = util.PriorityQueue()
@@ -205,7 +188,6 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
 
     #to be explored (FIFO): takes in item, cost+heuristic
     frontier = util.PriorityQueue()
